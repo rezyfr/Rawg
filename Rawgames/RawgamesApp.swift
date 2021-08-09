9194fbd28dedb1b29d45a24956493c20794476bc
Fidriyanto Rizkillah <fidriyanto.rizkillah@amartha.com>
2021-08-09 12:59:10 +0700

    Initial Commit

---
+++ Rawgames/RawgamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawgamesApp.swift
+//  Rawgames
+//
+//  Created by Fidriyanto R on 09/08/21.
+//
+
+import SwiftUI
+
+@main
+struct RawgamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
